ad34fa9e61e5b2a94ab974ed03e0c5c7b015a7b7
DariaSidorko <daria.sidorko@gmail.com>
2025-06-20 17:54:28 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, Node.js!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
